148dd325988c25bf723e4666ddeb0276ff65a7cd
julietteporte <juliette.porte@viacesi.fr>
2018-12-13 09:02:06 +0100

    IHM

---
+++ PPS_IHM/During.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PPS_IHM
+{
+    public partial class During : Form
+    {
+        public During()
+        {
+            InitializeComponent();
+        }
+    }
+}
--- PPS_IHM/Result.cs
+++ PPS_IHM/Result.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace PPS_IHM
 {
-    public partial class Form1 : Form
+    public partial class Result : Form
     {
-        public Form1()
+        public Result()
         {
             InitializeComponent();
         }
