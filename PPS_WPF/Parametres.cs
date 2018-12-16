6660e906f907d8c2a2ed50738efdc6d778c2f795
Charly PEYSSARD <charly.peyssard@viacesi.fr>
2018-12-16 22:30:48 +0100

    Test affichage Scenario

---
+++ PPS_WPF/Parametres.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPS_WPF
+{
+    public static class Parametres
+    {
+        public static int NumberClient { get; set; } = 1;
+        public static int NumberWaiters { get; set; } = 1;
+        public static int NumberCooks { get; set; } = 1;
+        public static int NumberScenario { get; set; } = 1;
+    }
+}
